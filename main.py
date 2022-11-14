89182e0ba55ccededa595f6e8929b0500ce6fc83
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2022-11-14 20:24:50 +0000

    pushes

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-# def my_sum(*args):
-#   print(*args)
+# # def my_sum(*args):
+# #   print(*args)
 
-# print(my_sum(3,4,5))
-# #args are for entering
-# #as many arguments as possible
+# # print(my_sum(3,4,5))
+# # #args are for entering
+# # #as many arguments as possible
+# # def a_sum(*args):
+# #   for num in args:
+# #     print (num)
+
+# # print(a_sum(1))
+# # print(a_sum(1,3))
+# # print(a_sum(1,3,4,5,6))
+# list = [2,4,6,3,7,1,5,8,9]
+# def sum_numbers(list):
+#   # sum = 0
+#   # for num in list:
+#   #   sum = sum + num
+#   # return sum 
+#   return sum(list)
+# print(sum_numbers(list))
+
+# #args
 # def a_sum(*args):
+#   total = 0
 #   for num in args:
-#     print (num)
-
-# print(a_sum(1))
-# print(a_sum(1,3))
-# print(a_sum(1,3,4,5,6))
-list = [2,4,6,3,7,1,5,8,9]
-def sum_numbers(list):
-  # sum = 0
-  # for num in list:
-  #   sum = sum + num
-  # return sum 
-  return sum(list)
-print(sum_numbers(list))
-
-#args
-def a_sum(*args):
-  # total = 0
-  # for num in args:
-  #   total += num  
-  return sum(args)
+#     total += num  
+#   return sum(args)
 
   
-  print(a_sum(3,5,23,1,6,7,42,27))
+#   print(a_sum(3,5,23,1,6,7,42,27))
+def a_sum(**kwargs):
+  total = 0
+  for key, value in kwargs.items():
+    print(f"{key} = {value}")
+    total += value
+  return total
+print(a_sum(x= 3,y= 5, z= 22))
